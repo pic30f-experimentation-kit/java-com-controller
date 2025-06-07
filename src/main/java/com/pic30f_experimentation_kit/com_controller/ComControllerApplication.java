f289bd4aacfbe6e174674f7d8dd383b09af2ce6d
Jean-Michel Gonet <jmgonet@yahoo.com>
2025-06-07 07:49:28 +0200

    Fresh download from https://start.spring.io/

---
+++ src/main/java/com/pic30f_experimentation_kit/com_controller/ComControllerApplication.java
@@ -0,0 +1,13 @@
+package com.pic30f_experimentation_kit.com_controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComControllerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pic30f_experimentation_kit/com_controller/ComControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pic30f_experimentation_kit.com_controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
